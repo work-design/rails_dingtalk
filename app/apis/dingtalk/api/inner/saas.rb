986139a16cf548dcb5e3eb09000c6f24ff96014e
qinmingyuan <mingyuan0715@foxmail.com>
2021-09-21 20:11:31 +0800

    typo

--- app/apis/dingtalk/api/inner/saas.rb
+++ app/apis/dingtalk/api/inner/saas.rb
@@ -32,7 +32,7 @@ module Dingtalk::Api
       payload = {
         receiverIds: Array(receiver_ids).join(','),
         tenantId: app.tenant_id,
-        msg: %Q({"msgtype":"text","text":{"content":#{text}}}')
+        msg: %Q({"msgtype":"text","text":{"content":#{text}}})
       }
 
       r = post '/message/workNotification', **payload
